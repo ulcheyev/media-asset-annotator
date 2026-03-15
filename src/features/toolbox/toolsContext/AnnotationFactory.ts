a708dd4791fc1bee29281301a7c2d81319e91e66
Yevgeniy <ulchenkov2001@gmail.com>
2026-03-15 15:04:17 +0100

    [fix] fix path to env

--- src/features/toolbox/toolsContext/AnnotationFactory.ts
+++ src/features/toolbox/toolsContext/AnnotationFactory.ts
@@ -1,11 +1,11 @@
 import type { ToolContextInterface } from './ToolContextInterface.ts';
 import { Constants } from '../../../utils/Constants.ts';
-import {generateUUID} from "../../../utils/base.utils.ts";
+import { generateUUID } from '../../../utils/base.utils.ts';
 
 export class AnnotationFactory {
   static createBase(ctx: ToolContextInterface, label: string) {
     const id = generateUUID();
-    console.log(id)
+    console.log(id);
     const { currentTime, duration } = ctx.getTimeContext();
 
     return {
